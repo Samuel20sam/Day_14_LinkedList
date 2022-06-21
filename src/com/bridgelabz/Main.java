5811f4bf0358db7c5522025aae4f8ee4e397e2e0
Samuel_Elijah <samuel20sam@gmail.com>
2022-06-21 07:48:24 +0530

    Adding: Add method

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+        // welcome to linked list program.
+
+        LinkedList list = new LinkedList();
+        list.add(70);
+        list.add(56);
+        list.add(30);
+        list.display();
+    }
+}
---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Node {
+    int data;
+    Node next;
+
+    // Parameter Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
