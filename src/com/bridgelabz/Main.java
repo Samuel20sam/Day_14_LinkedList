d2eae57cdd6f93d61deea63b4f35b7a71865c9bd
Samuel_Elijah <samuel20sam@gmail.com>
2022-06-26 07:51:14 +0530

    Adding ability to insert

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Type four integer");
-        int x1 = input.nextInt(), x2 = input.nextInt(), x3 = input.nextInt(), x4 = input.nextInt();
+        System.out.println("Type three integer");
+        int x1 = input.nextInt(), x2 = input.nextInt(), x3 = input.nextInt();
+//                x4 = input.nextInt();
 
         LinkedList<Integer> list = new LinkedList<>();
         Node<Integer> fn = new Node<>(x1);
         Node<Integer> sn = new Node<>(x2);
         Node<Integer> tn = new Node<>(x3);
-        Node<Integer> fon = new Node<>(x4);
+//        Node<Integer> fon = new Node<>(x4);
 
 //        System.out.println("Add method");
 //        list.add(fn);
@@ -21,12 +22,16 @@ public class Main {
 
         System.out.println("Push method");
         list.push(fn);
-        list.push(sn);
+//        list.push(sn);
         list.push(tn);
         list.display();
 
-        System.out.println("\n\nInsert " + fon + " after " + x2);
-        list.insertAfter(x2, fon);
+//        System.out.println("\n\nInsert " + fon + " after " + x2);
+//        list.insertAfter(x2, fon);
+//        list.display();
+
+        System.out.println("\n\nInserting second element");
+        list.insert(sn);
         list.display();
 
 //        System.out.println("\n\nDelete first element (pop)");
